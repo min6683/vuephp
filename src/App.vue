7d52d84a7a8bd9e5e8bf90e9ee7764928ce4cf23
min6683 <dev.sm6683@gmail.com>
2022-05-05 21:35:36 +0900

    vue php버전 업데이트

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+
+    <nav>
+      <router-link :to="{name:'Home'}">Home</router-link> |
+      <router-link :to="{name:'Create'}">Ceate</router-link> |
+      <router-link :to="{name:'List'}">List</router-link> |
+      <router-link :to="{name:'About'}">About</router-link>
+    </nav>
+
+    <router-view />
+    <!-- 안내창 -->
+    <transition name="fade">
+      <ToastBox :message="toastMessage" v-if="toastShow"/>
+    </transition>
+  </div>
+
+
+</template>
+
+<script>
+  import {useToast} from '@/composables/toast.js'
+import ToastBox from './components/ToastBox.vue';
+  export default {
+  components: { ToastBox },
+    setup() {
+      const {
+        toastMessage,
+        toastShow,
+        triggerToast
+      } = useToast();
+
+      return {
+        toastMessage,
+        toastShow,
+        triggerToast
+      }
+    }
+  }
+  
+</script>
+
+<style>
+  #app {}
+
+  .fade-enter-active,
+  .fade-leave-active{
+    transition:opactiy 0.5s;
+  } 
+  .fade-enter-from,
+  .fade-leave-to{
+    opacity: 0;
+  }
+  .fade-enter-to,
+  .fade-leave-from{
+    opacity: 1;
+  }
+</style>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>안녕하세요:)</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h1{
+  position: absolute;
+  left: 50%;
+  top:50%;
+  transform: translate(-50%, -50%);
+}
+</style>
